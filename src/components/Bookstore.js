61146c651522aa4fbbc0aadd1033a3bba3b29101
Zibilyonik <onatsurmeli@gmail.com>
2021-08-24 16:34:42 +0200

    Add linters to project

--- src/components/Bookstore.js
+++ src/components/Bookstore.js
@@ -3,18 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Calculator.css';
 
-const Bookstore = () => {
-
-  return (
-    <Switch>
-        <Route exact path="/">
-          <div id="Books" />
-        </Route>
-        <Route path="/categories">
-          <h1>Under Construction</h1>
-        </Route>
-      </Switch>
-  )
-}
+const Bookstore = () => (
+  <Switch>
+    <Route exact path="/">
+      <div id="Books" />
+    </Route>
+    <Route path="/categories">
+      <h1>Under Construction</h1>
+    </Route>
+  </Switch>
+);
 
 export default Bookstore;
