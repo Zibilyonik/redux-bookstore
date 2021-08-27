11a3ea974700e001893d4f689f4b94ae8d5cd136
Zibilyonik <onatsurmeli@gmail.com>
2021-08-28 00:56:15 +0200

    Add CallAPI file to set up API functionality - Create SET_LIST reducer for receiving data from API - Add thunk to configureStore.js

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,11 @@
 const ADD_BOOK = 'redux-bookstore/src/redux/books/ADD_BOOK';
 const REMOVE_BOOK = 'redux-bookstore/src/redux/books/REMOVE_BOOK';
+const SET_LIST = 'redux-bookstore/src/redux/books/SET_LIST';
 const initialState = [];
 
+export const setList = () => ({
+  type: SET_LIST,
+});
 export const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
@@ -13,14 +17,16 @@ export const removeBook = (id) => ({
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_BOOK: return [
-      ...state,
-      {
-        title: action.payload.title,
-        id: action.payload.id,
-        author: action.payload.author,
-      },
-    ];
+    case SET_LIST: return action;
+    case ADD_BOOK:
+      return [
+        ...state,
+        {
+          title: action.payload.title,
+          id: action.payload.id,
+          author: action.payload.author,
+        },
+      ];
     case REMOVE_BOOK:
       return [...state.filter((book) => book.id !== action.id)];
     default:
